cd7ec2d91f5167ac433761c1cea0d82891818882
Amelia Lloyd <amelialloyd31@gmail.com>
2020-05-17 12:57:32 +0100

    Adding logic to function

--- timeConverter.js
+++ timeConverter.js
@@ -1,7 +1,13 @@
 function timeConverter(seconds) {
     //convert seconds to minutes
     //add minutes string
-    return (seconds / 60) + " minutes"
+    let minutes = seconds/60
+    if (minutes%5) {
+        return minutes + (5-(minutes%5)) + ""
+    }
+    else {
+        return minutes + " minutes"
+    }
 }
 
 module.exports = timeConverter
