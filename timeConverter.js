b1b3ebb3e5e8cfd3f685111107f74d47e562f4ff
Amelia Lloyd <amelialloyd31@gmail.com>
2020-05-17 13:12:11 +0100

    Update comments

--- timeConverter.js
+++ timeConverter.js
@@ -1,10 +1,11 @@
 function timeConverter(seconds) {
-    //convert seconds to minutes
-    //add minutes string
+    //convert minutes to seconds
     let minutes = seconds/60
+    //if minutes isn't multiple of 5
     if (minutes%5) {
         return minutes + (5-(minutes%5)) + " minutes"
     }
+    //if minutes is multiple of 5
     else {
         return minutes + " minutes"
     }
